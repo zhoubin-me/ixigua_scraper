b6420c2ef3185d3a0fd841fe7e194848e1e14e08
ZHOU Bin <zhoubin.me@gmail.com>
2021-12-03 22:31:28 +0800

    update

--- bb.py
+++ bb.py
@@ -1,19 +1,95 @@
+import shutil
 import time
 import os
 import re
 import requests
-from selenium import webdriver
+# from selenium import webdriver
+from seleniumwire import webdriver
 from selenium.webdriver.chrome.options import Options
+from seleniumwire.request import Response
 from webdriver_manager.chrome import ChromeDriverManager
+import wget
+from videoprops import get_video_properties
 
-html_url = 'https://www.ixigua.com/embed?group_id=7036308375272948236'
+# html_url = 'https://www.ixigua.com/7036308375272948236'
+
 
 chrome_options = Options()
 chrome_options.add_argument('--headless')
-driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=chrome_options)
-driver.get(html_url)
-driver.implicitly_wait(10)
-elems = driver.find_elements_by_xpath('//xg-definition/ul/li')
-import pdb; pdb.set_trace()
-for elem in elems:
-    print(elem)
+chrome_options.add_argument('--user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0.3 Safari/605.1.15"')
+
+
+title_dict = {}
+
+with open('vlist.txt', 'r', encoding='utf-8') as f:
+    lines = f.readlines()
+    for line in lines:
+        title, url = line.split('\t')
+        url = 'https://www.ixigua.com' + url.strip()
+        title = title.strip()
+        title_dict[url] = title
+
+print(title_dict)
+
+
+def download_video(title, html_url):
+    if os.path.exists("D:\\Colorful\\" + title + '.mp4'):
+        return
+    driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=chrome_options)
+    driver.get(html_url)
+    driver.implicitly_wait(5)
+    elems = driver.find_elements_by_xpath('//div[@class="commentList"]')
+    print(elems)
+
+    video_links = {}
+    max_video_size = -1
+    max_video_link = None
+    max_audio_size = -1
+    max_audio_link = None
+    for request in driver.requests:
+        if request.response:
+            if request.response.headers['Content-Type'] == 'video/mp4':
+                new_url, _ = request.url.split('&net=')
+                media_size = int(request.response.headers['Content-Range'].split('/')[-1])
+
+
+                if 'media-video-avc1' in new_url:
+                    media_type = 'video'
+                    if media_size > max_video_size:
+                        max_video_link = new_url
+                        max_video_size = media_size
+                else:
+                    media_type = 'audio'
+                    if media_size > max_audio_size:
+                        max_audio_link = new_url
+                        max_audio_size = media_size
+                
+                if new_url not in video_links:
+                    video_links[new_url] = (media_type, media_size)
+
+                
+    print(video_links)
+    for k, info in video_links.items():
+        print("-" * 20)
+        print(k)
+        print(info)
+        print("=" * 20)
+    driver.close()
+    del driver
+    print(title)
+    wget.download(max_audio_link, out='D:\\Colorful\\' + title + '.mp3')
+    print(title)
+    wget.download(max_video_link, out='D:\\Colorful\\' + title + '_.mp4')
+
+    if os.path.exists('D:\\Colorful\\' + title + '_.mp4') and os.path.exists('D:\\Colorful\\' + title + '.mp3'):
+        os.system(f'ffmpeg -i "D:\\Colorful\\{title}_.mp4" -i "D:\\Colorful\\{title}.mp3" -c copy "D:\\Colorful\\{title}.mp4"')
+        os.remove('D:\\Colorful\\' + title + '.mp3')
+        os.remove('D:\\Colorful\\' + title + '_.mp4')
+
+
+while True:
+    for html_url, title in title_dict.items():
+        try:
+            download_video(title, html_url)
+        except:
+            pass
--- ixigua/spiders/example.py
+++ ixigua/spiders/example.py
@@ -1,10 +1,11 @@
 import scrapy
 from scrapy_splash import SplashRequest
-from selenium import webdriver
+from seleniumwire import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-import wget
+import wget, os, shutil
+from videoprops import get_video_properties
 
 
 script = """
@@ -42,7 +43,7 @@ class VListSpider(scrapy.Spider):
         elems = response.xpath('//a[@class="HorizontalFeedCard__coverWrapper disableZoomAnimation"]')
         print(len(elems))
 
-        with open('vlist.txt', 'w') as f:
+        with open('vlist.txt', 'w', encoding='utf-8') as f:
             for elem in elems:
                 title = elem.attrib['title']
                 url = elem.attrib['href']
@@ -54,30 +55,85 @@ class VideoSpider(scrapy.Spider):
     allowed_domains = ['ixigua.com']
     title_dict = {}
     def start_requests(self):
-        with open('vlist.txt', 'r') as f:
+        with open('vlist.txt', 'r', encoding='utf-8') as f:
             lines = f.readlines()
         
 
         for line in lines:
+            if len(line.strip()) == 0:
+                continue
             title, url = line.split('\t')
-            url = 'https://www.ixigua.com/embed?group_id=' + url.strip()[1:]
+            url = 'https://www.ixigua.com' + url.strip()
             title = title.strip()
             self.title_dict[url] = title
 
-        chrome_options = Options()
-        chrome_options.add_argument('--headless')
-        self.driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=chrome_options)
-        for url in self.title_dict.keys():
+        print("= * 20")
+        print(self.title_dict)
+        for url, title  in self.title_dict.items():
             yield scrapy.Request(url=url, callback=self.parse)
 
 
     def parse(self, response):
-        self.driver.get(response.url)
-        self.driver.implicitly_wait(5)
-        elems = self.driver.find_elements_by_xpath('//xg-definition/ul/li')
-        video_url = elems[1].get_attribute('url')
         title = self.title_dict[response.url]
-        wget.download('https:' + video_url, out=title + '.mp4')
+        html_url = response.url
+        if os.path.exists("D:\\Colorful\\" + title + '.mp4'):
+            return
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0.3 Safari/605.1.15"')
+
+        driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=chrome_options)
+        driver.get(html_url)
+        driver.implicitly_wait(3)
+        elems = driver.find_elements_by_xpath('//div[@class="commentList"]')
+        print(elems)
+
+        video_links = {}
+        max_video_size = -1
+        max_video_link = None
+        max_audio_size = -1
+        max_audio_link = None
+        for request in driver.requests:
+            if request.response:
+                if request.response.headers['Content-Type'] == 'video/mp4':
+                    new_url, _ = request.url.split('&net=')
+                    media_size = int(request.response.headers['Content-Range'].split('/')[-1])
+
+
+                    if 'media-video-avc1' in new_url:
+                        media_type = 'video'
+                        if media_size > max_video_size:
+                            max_video_link = new_url
+                            max_video_size = media_size
+                    else:
+                        media_type = 'audio'
+                        if media_size > max_audio_size:
+                            max_audio_link = new_url
+                            max_audio_size = media_size
+                    
+                    if new_url not in video_links:
+                        video_links[new_url] = (media_type, media_size)
+
+                    
+        print(video_links)
+        for k, info in video_links.items():
+            print("-" * 20)
+            print(k)
+            print(info)
+            print("=" * 20)
+        driver.close()
+        del driver
+        print(title)
+        wget.download(max_audio_link, out='D:\\Colorful\\' + title + '.mp3')
+        print(title)
+        wget.download(max_video_link, out='D:\\Colorful\\' + title + '_.mp4')
+
+        if os.path.exists('D:\\Colorful\\' + title + '_.mp4') and os.path.exists('D:\\Colorful\\' + title + '.mp3'):
+            os.system(f'ffmpeg -i "D:\\Colorful\\{title}_.mp4" -i "D:\\Colorful\\{title}.mp3" -c copy "D:\\Colorful\\{title}.mp4"')
+            os.remove('D:\\Colorful\\' + title + '.mp3')
+            os.remove('D:\\Colorful\\' + title + '_.mp4')
+
+
 
 class ExampleSpider(scrapy.Spider):
     name = 'example'
